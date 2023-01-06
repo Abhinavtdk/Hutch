1837fc214c13ad6378841da688a96486e9127be6
gk-abhinav <abhinav.ch@greatkapital.com>
2023-01-07 03:28:57 +0530

    Added backend

---
+++ backend/src/main/java/com/example/demo/HutchbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HutchbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HutchbackendApplication.class, args);
+	}
+
+}
